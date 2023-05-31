3dd69ff9aa6f222401374223a8b92659a82225bb
aevocaedoes <100850357+aevocaedoes@users.noreply.github.com>
2023-05-31 18:58:40 +0700

    Update index.php

--- index.php
+++ index.php
@@ -5,32 +5,60 @@
   <style>
     body {
       font-family: Arial, sans-serif;
-      margin: 20px;
+      margin: 0;
+      padding: 0;
+      background-color: lavenderblush;
     }
 
-    h1 {
-      color: #333;
+    .container {
+      max-width: 600px;
+      margin: 0 auto;
+      padding: 40px;
+      background-color: #fff;
+      border-radius: 8px;
+      box-shadow: 0 2px 6px rgba(0, 0, 0, 0.1);
     }
 
     .profile-image {
       width: 200px;
-      height: auto;
+      height: 200px;
       border-radius: 50%;
       margin-bottom: 20px;
+      object-fit: cover;
+      object-position: center;
+      box-shadow: 0 2px 6px rgba(0, 0, 0, 0.1);
+    }
+
+    h1 {
+      color: #333;
+      margin-bottom: 20px;
+      text-align: center;
+    }
+
+    h2 {
+      color: #666;
+      margin-bottom: 10px;
+    }
+
+    p {
+      color: #333;
+      line-height: 1.5;
     }
   </style>
 </head>
 <body>
-  <h1>Profil Saya</h1>
-  <img class="profile-image" src="https://storage.googleapis.com/my-bucket/profile-image.jpg" alt="Foto Profil">
+  <div class="container">
+    <h1>Profil Saya</h1>
+    <img class="profile-image" src="dhymas.JPEG" alt="Foto Profil">
 
-  <h2>Nama:</h2>
-  <p>John Doe</p>
+    <h2>Nama:</h2>
+    <p>Dhymas Bagus Tri Pamungkas</p>
 
-  <h2>Alamat:</h2>
-  <p>Jl. Contoh No. 123, Kota Contoh</p>
+    <h2>Alamat:</h2>
+    <p>Kulonprogo, Yogyakarta</p>
 
-  <h2>Deskripsi:</h2>
-  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vel ex bibendum, semper lectus sit amet, placerat tellus.</p>
+    <h2>Deskripsi:</h2>
+    <p>saya adalah seorang mahasiswa semester 6 Program Studi Informatika di UIN Sunan Kalijaga Yogyakarta</p>
+  </div>
 </body>
 </html>
